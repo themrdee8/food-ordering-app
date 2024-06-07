a1cb8ef7afe15574fee9d1badbd2294e07e15679
Senyo Dedoo <mrdee@Senyos-MacBook-Air.local>
2024-06-07 18:40:37 +0000

    2: Menu Screen and product lists

---
+++ src/components/ProductListItem.tsx
@@ -0,0 +1,25 @@
+import { Image, Text, View } from "react-native";
+import { Product } from "../types";
+
+type ProductListItemProps = {
+  product: Product;
+};
+
+export const defaultPizzaImage =
+  "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/default.png";
+
+const ProductListItem = ({ product }: ProductListItemProps) => {
+  return (
+    <View className="bg-white p-2 rounded-xl flex-1 max-w-[50%]">
+      <Image
+        source={{ uri: product.image || defaultPizzaImage }}
+        className="w-full aspect-[1]"
+        resizeMode="contain"
+      />
+      <Text className="text-[18px] font-[600] my-2">{product.name}</Text>
+      <Text className="text-[#2f95dc] font-bold">${product.price}</Text>
+    </View>
+  );
+};
+
+export default ProductListItem;
